c03dfa6c9f2659320451815e528f18154c12a128
DMyrzakanov <Danntu1986#>
2020-09-24 15:46:00 +0600

    init

---
+++ Test.cs
@@ -0,0 +1,17 @@
+using System;
+
+class Test
+{
+    public static void Main()
+    {
+        
+        Console.WriteLine(FeetToInch(30));
+        Console.WriteLine(FeetToInch(100));
+    }
+
+    static int FeetToInch(int feet){
+        return feet * 12;    
+    }
+
+    int Foo(int x)=> x*2;
+}
